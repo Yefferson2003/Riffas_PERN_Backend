3b1876a3b8f52e6e45fd6ebc4a1de67e069183df
yefferson2003 <ysantamaria@unicesar.edu.co>
2025-12-06 17:32:25 -0500

    update 6/12/2025 cors origin new

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,11 +3,15 @@ import { CorsOptions } from 'cors';
 export const corsConfig: CorsOptions = {
 
     origin: function (origin, callback) {
-    
-        const whitelist = [process.env.FRONTEND_URL];
-        
+        const whitelist = [
+            process.env.FRONTEND_URL,
+            process.env.FRONTEND_URL_PROD1,
+            process.env.FRONTEND_URL_PROD2,
+            process.env.FRONTEND_URL_PROD3,
+        ];
+
         if (process.argv[2] === '--api') {
-            whitelist.push(undefined)
+            whitelist.push(undefined);
         }
 
         if (whitelist.includes(origin)) {
